d75a66f2ccfeb924d91b9fb8ad8c522aea00e758
mtkrebs87 <144370108+mtkrebs87@users.noreply.github.com>
2023-11-28 19:17:33 -0500

    update to app.py

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 # Import the dependencies.
 
-
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, text
+from flask import Flask
 
 #################################################
 # Database Setup
 #################################################
 
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
+measurement = Base.classes.measurement
 
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
@@ -21,10 +30,12 @@
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def 
